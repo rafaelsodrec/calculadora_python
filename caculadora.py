0db657ce41a2d0f5a197dcf56b434fee00a82448
rafaelsodrec <112265564+rafaelsodrec@users.noreply.github.com>
2025-05-26 11:56:09 -0300

    Update caculadora.py

--- caculadora.py
+++ caculadora.py
@@ -1,9 +1,13 @@
+# Calculadora simples
+
+# Entrada dos números e da operação
 while True:
     num1 = int(input("Digite o Primeiro Numero: "))
     operacao = input("identifique a Operação: ")
     num2 = int(input("Digite o Segundo Numero: "))
     resultado = 0
 
+# Verificação da operação
     if operacao == "+":
         resultado = num1 + num2
         print("o resultado é", resultado)
